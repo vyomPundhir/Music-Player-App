f48f64ed96928af1c938aab30367bc9a2e3c3bb6
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-04 23:08:58 +0530

    tailwind config js file updated.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export const content = [
-  "./src/**/*.{html,ts}",
-];
-export const theme = {
-  extend: {},
+
+const scrollbar = require('tailwind-scrollbar');
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    scrollbar,
+  ],
 };
-export const plugins = [];
