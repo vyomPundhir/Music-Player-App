6c9e2ef4aee3fb66f476fb5ea6a0be1157368444
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-06 23:32:50 +0530

    app module ts file updated.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
+import { SearchModule } from './search/search.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HomeModule } from './home/home.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
+    SearchModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
