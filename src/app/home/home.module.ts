8ee13509b403598b95062068e53f89b8d95957ab
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-06 23:33:50 +0530

    home module ts file updated.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-
-
+import { AppRoutingModule } from '../app-routing.module';
+import { AllModule } from './home/all/all.module';
+import { MusicModule } from './home/music/music.module';
+import { PodcastsModule } from './home/podcasts/podcasts.module';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule,
+    AllModule,
+    MusicModule,
+    PodcastsModule
   ]
 })
 export class HomeModule { }
