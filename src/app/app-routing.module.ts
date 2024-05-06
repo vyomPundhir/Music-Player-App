e14e3e0e14318acfcdf413c6903222070eb39554
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2024-05-06 23:32:00 +0530

    app-routing module ts file updated.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
+import { AllComponent } from './home/home/all/all.component';
+import { MusicComponent } from './home/home/music/music.component';
+import { PodcastsComponent } from './home/home/podcasts/podcasts.component';
+import { SearchComponent } from './search/search/search.component';
 
 const routes: Routes = [
-  {path:'', component: HomeComponent}
+  {path:'home', component: HomeComponent, children: [
+    {
+      path: '', redirectTo: 'all', pathMatch: 'full'
+    },
+    {
+      path:'all', component: AllComponent
+    },
+    {
+      path:'music', component: MusicComponent
+    },
+    {
+      path: 'podcasts', component: PodcastsComponent
+    }
+  ]},
+  {path: 'search', component: SearchComponent},
+  {path:'', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
